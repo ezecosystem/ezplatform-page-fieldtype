1fe27cccb3c16e6e3770fccb8d1dd1d4f443e812
Damien Pobel <dp@ez.no>
2013-03-15 14:12:17 +0100

    Removed the page service from the objects available in ezpage value

--- tests/lib/FieldType/PageTest.php
+++ tests/lib/FieldType/PageTest.php
@@ -166,8 +166,8 @@ class PageTest extends FieldTypeTest
                 new PageValue()
             ),
             array(
-                new PageValue( new Page( $this->getPageServiceMock() ) ),
-                new PageValue( new Page( $this->getPageServiceMock() ) )
+                new PageValue( new Page() ),
+                new PageValue( new Page() )
             )
         );
     }
@@ -215,8 +215,8 @@ class PageTest extends FieldTypeTest
                 null
             ),
             array(
-                new PageValue( new Page( $this->getPageServiceMock() ) ),
-                serialize( new Page( $this->getPageServiceMock() ) ),
+                new PageValue( new Page() ),
+                serialize( new Page() ),
             )
         );
     }
@@ -264,8 +264,8 @@ class PageTest extends FieldTypeTest
                 null
             ),
             array(
-                serialize( new Page( $this->getPageServiceMock() ) ),
-                new PageValue( new Page( $this->getPageServiceMock() ) )
+                serialize( new Page() ),
+                new PageValue( new Page() )
             )
         );
     }
